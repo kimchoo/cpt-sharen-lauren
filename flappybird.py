4b09d1ed6a60af8d84965829d7b75acead9ddcd2
sharenchen <47427462+sharenchen@users.noreply.github.com>
2019-06-04 08:58:42 -0400

    Update flappybird.py

--- flappybird.py
+++ flappybird.py
@@ -1,1 +1,21 @@
+import arcade
 
+WIDTH = 350
+HEIGHT = 600
+
+jump_time = 0
+jump_time_cap = 100
+jump_speed = 10
+fall_speed = 4
+
+pos_x = 20
+pos_y = HEIGHT/2
+
+player_points = 0
+
+pipe_width = 8
+pipe_height = 5
+pipe_gap = 5
+pipe_speed = 3
+player_padding = 2
+pipes_on_screen_numb = 6
